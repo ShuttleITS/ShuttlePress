58c168e827d552b335f1414bf581caf9cb783d78
shuttleITS <i@xiamii.com>
2025-12-24 10:25:54 +0800

    修复了page的夜间模式

--- docs/.vuepress/.temp/internal/siteData.js
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -1,14 +1,1 @@
 export const siteData = JSON.parse("{\"base\":\"/ShuttlePress/\",\"lang\":\"zh-CN\",\"title\":\"Shuttle\",\"description\":\"Shuttle 前端主题的安装、配置与定制指南\",\"head\":[],\"locales\":{}}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateSiteData) {
-    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ siteData }) => {
-    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
-  })
-}
--- docs/.vuepress/.temp/pages/env.html.js
+++ docs/.vuepress/.temp/pages/env.html.js
@@ -1,16 +1,3 @@
 import comp from "/Users/yinian/Desktop/Shuttle/docs/.vuepress/.temp/pages/env.html.vue"
 const data = JSON.parse("{\"path\":\"/env.html\",\"title\":\"部署环境要求\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1765778259000,\"contributors\":[{\"name\":\"shuttleITS\",\"username\":\"shuttleITS\",\"email\":\"i@xiamii.com\",\"commits\":1,\"url\":\"https://github.com/shuttleITS\"}],\"changelog\":[{\"hash\":\"d24cde3e182d5bfc6af249a7e657bbdffed1897e\",\"time\":1765778259000,\"email\":\"i@xiamii.com\",\"author\":\"shuttleITS\",\"message\":\"first\"}]},\"filePathRelative\":\"env.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
